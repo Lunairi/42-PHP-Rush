3f0b5e9168ad4bd464a4dbfb2ccf0d6545d031c8
Michael Lu <mizukori250@gmail.com>
2017-10-08 14:31:35 -0700

    Add files via upload

--- basket.php
+++ basket.php
@@ -1,98 +1,72 @@
 <?php
-
-	session_start();
-
+    session_start();
 ?>
-
 <!DOCTYPE HTML>
-
 <html>
-
 <head>
-	<title>Best dirt in town</title>
-	<link rel="stylesheet" type="text/css" href="./styles/base.css">
-	<link rel="stylesheet" type="text/css" href="./styles/index.css">
+    <title>Best dirt in town</title>
+    <link rel="stylesheet" type="text/css" href="./styles/base.css">
+    <link rel="stylesheet" type="text/css" href="./styles/basket.css">
 </head>
-
 <body>
-
-	<nav>
-		<a href="./index.php">Dirt... just dirt.</a>
-		<?php
-			if ($_SESSION['logged_on'])
-			{
-				echo '<p class="userlog">';
-				echo $_SESSION['logged_on'];
-				echo '</p>';
-			}
-		?>
-		<ul><a href="./index.php">Dirt<img src="./src/logo.png" /></a>
-			<div class="dropdown-content">
-			<?php
-				if (!$_SESSION['logged_on'])
-					echo'<a href="./login.php"><li>Login</li></a>';
-				else
-					echo'<a href="./logout.php"><li>Logout</li></a>';
-			?>
-				<a href="./settings.php"><li>Settings</li></a>
-				<a href="./basket.php"><li>Basket</li></a>
-				<a href="./checkout.php"><li>Checkout</li></a>
-			</div>
-		</ul>
-	</nav>
-
-	<div class="main-container catalog">
-
-	<?php
-
-	if ($_SESSION['add'] == 1)
-	{
-		echo '<div>Product has been added to cart.</div><br>';
-		$_SESSION['add'] = "";
-	}
-	else if ($_SESSION['add'] == 2)
-	{
-		echo '<div>No product has been added.</div><br>';
-		$_SESSION['add'] = "";
-	}
-	if (!$_SESSION['logged_on'])
-		echo "You must be logged in to view this.";
-	else
-	{
-		$basket = unserialize(file_get_contents('./secure/basket'));
-		foreach ($basket as $key => $arg) 
-		{
-			if ($arg['login'] === $_SESSION['logged_on'])
-				$cart = 1;
-			if ($cart && !$disp)
-			{
-				$disp = 1;
-				if ($basket[$key]['soil1'] > 0)
-					echo "Magic: " . $basket[$key]['soil1'] . ".\n\n";
-				if ($basket[$key]['soil2'] > 0)
-					echo "Serenity: " . $basket[$key]['soil2'] . ".\n\n";
-				if ($basket[$key]['soil3'] > 0)
-					echo "Tranquilty: " . $basket[$key]['soil3'] . ".\n\n";
-				if ($basket[$key]['soil4'] > 0)
-					echo "Transformation: " . $basket[$key]['soil4'] . ".\n\n";
-				if ($basket[$key]['soil5'] > 0)
-					echo "Freedom: " . $basket[$key]['soil5'] . ".\n\n";
-				if ($basket[$key]['soil6'] > 0)
-					echo "Determination: " . $basket[$key]['soil6'] . ".\n\n";
-				if ($basket[$key]['soil7'] > 0)
-					echo "Might: " . $basket[$key]['soil7'] . ".\n\n";
-				if ($basket[$key]['soil8'] > 0)
-					echo "Guilt: " . $basket[$key]['soil8'] . ".\n\n";
-				if ($basket[$key]['soil9'] > 0)
-					echo "Purity: " . $basket[$key]['soil9'] . ".\n\n";
-			}
-		}
-	}
-
-	?>
-
-	</div>
-
+    <nav>
+        Dirt... just dirt.
+        <ul><a href="./index.php">Dirt<img src="./src/logo.png" /></a>
+            <div class="dropdown-content">
+                <a href="./login.php"><li>Login</li></a>
+                <a href="./settings.php"><li>Settings</li></a>
+                <a href="./basket.php"><li>Basket</li></a>
+                <a href="./checkout.php"><li>Checkout</li></a>
+            </div>
+        </ul>
+    </nav>
+    <form class="main-container catalog" action="update.php">
+        <div class="title">Basket</div>
+        <table>
+            <tr class="table-title"><td>Item</td><td>Quantity</td><td>Price</td></tr>
+<?php
+    if (!$_SESSION['logged_on'])
+        echo "You must be logged in to view this.";
+    else
+    {
+        $basket = unserialize(file_get_contents('./secure/basket'));
+        foreach ($basket as $key => $arg) 
+        {
+            if ($arg['login'] === $_SESSION['logged_on'])
+                $cart = 1;
+            if ($cart && !$disp)
+            {
+                $disp = 1;
+                if ($basket[$key]['soil1'] > 0)
+                    echo '<tr><td>Magic: </td><td><input type="number" min="0" value="' . $basket[$key]['soil1'] . '" /></td><td>' . $basket[$key]['price1'] . '</td></tr>';
+                if ($basket[$key]['soil2'] > 0)
+                    echo '<tr><td>Serenity: </td><td><input type="number" min="0" value="' . $basket[$key]['soil2'] . '" /></td><td>' . $basket[$key]['price2'] . '</td></tr>';
+                if ($basket[$key]['soil3'] > 0)
+                    echo '<tr><td>Tranquility: </td><td><input type="number" min="0" value="' . $basket[$key]['soil3'] . '" /></td><td>' . $basket[$key]['price3'] . '</td></tr>';
+                if ($basket[$key]['soil4'] > 0)
+                    echo '<tr><td>Transformation: </td><td><input type="number" min="0" value="' . $basket[$key]['soil4'] . '" /></td><td>' . $basket[$key]['price4'] . '</td></tr>';
+                if ($basket[$key]['soil5'] > 0)
+                    echo '<tr><td>Freedom: </td><td><input type="number" min="0" value="' . $basket[$key]['soil5'] . '" /></td></tr>';
+                if ($basket[$key]['soil6'] > 0)
+                    echo '<tr><td>Magic: </td><td><input type="number" min="0" value="' . $basket[$key]['soil1'] . '" /></td><td>' . $basket[$key]['price1'] . '</td></tr>';
+                if ($basket[$key]['soil7'] > 0)
+                    echo '<tr><td>Determination: </td><td><input type="number" min="0" value="' . $basket[$key]['soil6'] . '" /></td><td>' . $basket[$key]['price6'] . '</td></tr>';
+                if ($basket[$key]['soil8'] > 0)
+                    echo '<tr><td>Might: </td><td><input type="number" min="0" value="' . $basket[$key]['soil8'] . '" /></td><td>' . $basket[$key]['price8'] . '</td></tr>';
+                if ($basket[$key]['soil9'] > 0)
+                    echo '<tr><td>Purity: </td><td><input type="number" min="0" value="' . $basket[$key]['soil9'] . '" /></td><td>' . $basket[$key]['price9'] . '</td></tr>';
+            }
+        }
+    }
+?>
+        <tr><td>Total: </td><td></td><td></td>
+        </table>
+    
+            <tr>
+                <td><input type="submit" name="update" value="Edit Cart" /></td>
+                <td><a href="./checkout.php">Checkout</a></td>
+            </tr>'
+        
+    </form>
 </body>
-
 </html>
---
+++ checkout.php
@@ -0,0 +1,86 @@
+<?php
+
+	session_start();
+	if ($_SESSION['logged_on'])
+	{
+		if ($_POST['amount'])
+		{
+			$basket = unserialize(file_get_contents('./secure/basket'));
+			$checkout = unserialize(file_get_contents('./secure/checkout'));
+			$temp['login'] = $_SESSION['logged_on'];
+			foreach ($basket as $key => $arg)
+			{
+				if ($arg['login'] === $_SESSION['logged_on'])
+					$cart = 1;
+				if ($cart && !$buy)
+				{
+					$buy = 1;
+					if ($_POST['soil1'] > 0)
+					{
+						$temp['soil1'] = $basket[$key]['soil1'] + ($_POST['soil1'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil2'] > 0)
+					{
+						$temp['soil2'] = $basket[$key]['soil2'] + ($_POST['soil2'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil3'] > 0)
+					{
+						$temp['soil3'] = $basket[$key]['soil3'] + ($_POST['soil3'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil4'] > 0)
+					{
+						$temp['soil4'] = $basket[$key]['soil4'] + ($_POST['soil4'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil5'] > 0)
+					{
+						$temp['soil5'] = $basket[$key]['soil5'] + ($_POST['soil5'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil6'] > 0)
+					{
+						$temp['soil6'] = $basket[$key]['soil6'] + ($_POST['soil6'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil7'] > 0)
+					{
+						$temp['soil7'] = $basket[$key]['soil7'] + ($_POST['soil7'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil8'] > 0)
+					{
+						$temp['soil8'] = $basket[$key]['soil8'] + ($_POST['soil8'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_POST['soil9'] > 0)
+					{
+						$temp['soil9'] = $basket[$key]['soil9'] + ($_POST['soil9'] * 1);
+						$_SESSION['buy'] = 1;
+					}
+					if ($_SESSION['buy'] == 1)
+					{
+						$purchase[] = $temp;
+						file_put_contents('./secure/checkout', serialize($purchase));
+						foreach ($basket as $key2 => $arg2) 
+							if ($arg2['login'] === $_POST['login']) 
+							{
+								unset($basket[$key2]);
+								file_put_contents('./secure/basket', serialize($basket));
+							}
+					}
+				}
+			}
+			if ($_SESSION['buy'] != 1)
+			{
+				echo "There is nothing to checkout.";
+			}
+		}
+		else
+			$_SESSION['buy'] = 2;
+	}
+	header('Location: basket.php');
+	exit;
+?>
